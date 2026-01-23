bf043d3569aad582b8fd4228824f33e4e9dacf8e
p4xx07 <50495900+p4xx07@users.noreply.github.com>
2026-01-23 15:07:59 +0100

    added flash

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -121,6 +121,7 @@ vim.pack.add({
   { src = "https://github.com/mason-org/mason.nvim" },
   { src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" }, -- depends on mini, treesitter
   { src = "https://github.com/nvim-treesitter/nvim-treesitter-context" },
+  { src = "https://github.com/folke/flash.nvim"},
 
   -- debug
   { src = "https://github.com/mfussenegger/nvim-dap" },
---
+++ nvim/.config/nvim/plugins/flash.lua
@@ -0,0 +1,8 @@
+require("flash").setup({})
+
+local flash = require("flash")
+
+vim.keymap.set({ "n", "x", "o" }, "s", function()  flash.jump() end, { desc = "Flash" })
+vim.keymap.set({ "n", "x", "o" }, "S", function() flash.treesitter() end, { desc = "Flash Treesitter" })
+vim.keymap.set("o", "r", function() flash.remote() end, { desc = "Remote Flash" })
+vim.keymap.set({ "o", "x" }, "R", function() flash.treesitter_search() end, { desc = "Treesitter Search" })
